af15f100d24278b768f223fa76cc482ed698e1e5
Philip Krück <46320394+philipkrck@users.noreply.github.com>
2021-10-14 14:51:48 +0200

    made UIButton.swift available in Objective C

--- Sources/APLUIKitExtension/UIButton.swift
+++ Sources/APLUIKitExtension/UIButton.swift
@@ -5,13 +5,14 @@
 
 import UIKit
 
+@objc
 public extension UIButton {
     
     /// Set a state-dependent background color for a button. UIButton by default
     /// can only be configured with a state-dependent background image.
     /// - Parameter color: the color to set
     /// - Parameter forState: the state to set the color for
-    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+    @objc func setBackgroundColor(color: UIColor, forState: UIControl.State) {
         UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
         
         guard let context = UIGraphicsGetCurrentContext() else { return }
@@ -29,7 +30,7 @@ public extension UIButton {
     /// can only be configured with a state-dependent background image.
     /// - Parameter color: the color to set
     /// - Parameter forState: the state to set the color for
-    func setRoundedRectBackgroundImage(color: UIColor, height: CGFloat, cornerRadius: CGFloat, forState: UIControl.State) {
+    @objc func setRoundedRectBackgroundImage(color: UIColor, height: CGFloat, cornerRadius: CGFloat, forState: UIControl.State) {
         UIGraphicsBeginImageContextWithOptions(CGSize(width: height, height: height), false, 0)
         
         guard let context = UIGraphicsGetCurrentContext() else { return }
@@ -54,7 +55,7 @@ public extension UIButton {
     /// - Parameter startColor: the start color of the gradient color
     /// - Parameter endColor: the end color of the gradient color
     /// - Parameter forState: the state to set the color for
-    func setGradientBackgroundColor(startColor: UIColor, endColor: UIColor, forState: UIControl.State) {
+    @objc func setGradientBackgroundColor(startColor: UIColor, endColor: UIColor, forState: UIControl.State) {
         let inset: CGFloat = 2 // stretch a two pixels wide area, UIImage.gradient creates an image wide enough
         let gradientImage = UIImage.gradientImage(startColor: startColor, endColor: endColor).resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: inset), resizingMode: .stretch)
         
